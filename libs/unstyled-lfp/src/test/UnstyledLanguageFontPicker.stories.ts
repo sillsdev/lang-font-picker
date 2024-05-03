9c7a03fa460bb838807a76773c2b26eb6d6a448b
D. Ror <imnasnainaec@gmail.com>
2024-05-03 09:21:29 -0400

    [unstyled-lfp] Add to stories: css, multiselect demo (#15)

--- libs/unstyled-lfp/src/test/UnstyledLanguageFontPicker.stories.ts
+++ libs/unstyled-lfp/src/test/UnstyledLanguageFontPicker.stories.ts
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import "./stories.css";
+
 import UnstyledLanguageFontPicker from "../UnstyledLanguageFontPicker";
 
 // This default export determines where your story goes in the story list
@@ -11,9 +13,24 @@ export default meta;
 
 type Story = StoryObj<typeof UnstyledLanguageFontPicker>;
 
-export const ExtraFonts: Story = {
+export const MultiselectOff: Story = {
   args: {
     options: {
+      allowMultiselect: false,
+      extraFonts: [
+        { name: "Font Sans" },
+        { name: "Font Serif" },
+        { name: "Noto Font" },
+        { name: "Font SIL" },
+      ],
+    },
+  },
+};
+
+export const MultiselectOn: Story = {
+  args: {
+    options: {
+      allowMultiselect: true,
       extraFonts: [
         { name: "Font Sans" },
         { name: "Font Serif" },
